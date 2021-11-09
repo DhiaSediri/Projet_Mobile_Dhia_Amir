026d99e48bd2549a6da1e6190a596e1d00482b37
DhiaSediri <47246647+DhiaSediri@users.noreply.github.com>
2021-11-09 12:18:00 +0100

    Sending Data to new activity

--- app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
@@ -1,13 +1,51 @@
 package com.teamtreehouse.interactivestory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
